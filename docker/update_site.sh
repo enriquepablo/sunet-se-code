4df97ca003b722645e58ca7d75e5d91a54490970
enriquepablo <enrique@cazalla.net>
2024-03-13 19:17:42 +0100

    git pull

--- docker/update_site.sh
+++ docker/update_site.sh
@@ -4,8 +4,17 @@
 cd /opt/sunet-se-code || exit 1
 
 # Pull changes from the remote origin, including submodules
-git pull --recurse-submodules origin main >/dev/null 2>&1
+git pull >/dev/null 2>&1
 pull_status=$?
+
+# Change directory to /opt/web
+cd /opt/sunet-se-code/sunet-se-content || exit 1
+
+# Pull changes from the remote origin, including submodules
+git pull >/dev/null 2>&1
+pull_status_2=$?
+
+cd ..
 
 get-jira-tickets.sh -c get-jira-tickets.conf -p "$SUNET_JIRA_PASSWORD"
 tickets_status=$?
@@ -18,7 +27,7 @@ make pristine >/dev/null 2>&1
 make_status=$?
 
 # If there were errors in either the git pull or make commands, return exit status 1
-if [ $pull_status -ne 0 ] || [ $tickets_status -ne 0 ] || [ $make_status -ne 0 ]; then
+if [ $pull_status -ne 0 ] || [ $pull_status_2 -ne 0 ] || [ $tickets_status -ne 0 ] || [ $make_status -ne 0 ]; then
     exit 1
 fi
 
