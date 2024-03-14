41fd763ad5ec2f4417bd9a28b99883af4feaad96
enriquepablo <enrique@cazalla.net>
2024-03-14 10:11:50 +0100

    update site

--- docker/update_site.sh
+++ docker/update_site.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
+#
+touch update_site.log
 
 # Change directory to /opt/web
 cd /opt/sunet-se-code || exit 1
 
-# Pull changes from the remote origin, including submodules
-git pull >/dev/null 2>&1
+# Pull changes from the remote origin
+git pull &>update_site.log
 pull_status=$?
 
 # Change directory to /opt/web
 cd /opt/sunet-se-code/sunet-se-content || exit 1
 
-# Pull changes from the remote origin, including submodules
-git pull >/dev/null 2>&1
+git checkout GIT_BRANCH &>>update_site.log
+git fetch --all &>>update_site.log
 pull_status_2=$?
+
+git reset --hard origin/GIT_BRANCH &>>update_site.log
+git checkout GIT_BRANCH &>>update_site.log
+git pull &>>update_site.log
 
 cd ..
 
-get-jira-tickets.sh -c get-jira-tickets.conf -p "$SUNET_JIRA_PASSWORD"
+get-jira-tickets.sh -c get-jira-tickets.conf -p SUNET_JIRA_PASSWORD &>>update_site.log
 tickets_status=$?
 
 # activate virtualenv
 source venv/bin/activate
 
 # Run "make pristine" to build the site
-make pristine >/dev/null 2>&1
+make pristine &>>update_site.log
 make_status=$?
 
 # If there were errors in either the git pull or make commands, return exit status 1
